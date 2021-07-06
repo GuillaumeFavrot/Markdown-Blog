8456c383947cc8b526a37fb52ff08764a0548fc6
Guthan-FR <gui.favrot@gmail.com>
2021-07-06 18:07:15 +0200

    modification DB

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
 const app = express()
 
-mongoose.connect('mongodb://localhost/blog', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+mongoose.connect('mongodb+srv://Guillaume:Guigui845@cluster0.qicsa.mongodb.net/Markdownblog?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended : false}))
